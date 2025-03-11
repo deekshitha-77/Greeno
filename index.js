db96eb9816d293d86657806dd4d905722aae79b9
i578046 <sanka.deekshitha@sap.com>
2025-03-11 19:15:46 +0530

    test_coverage

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 app.use(morgan('dev'));
 app.use(express.static('public'));
