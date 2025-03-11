63a3eb2c6f5b5e6e818b2202d09fb4563982d7c6
i578046 <sanka.deekshitha@sap.com>
2025-03-11 22:23:08 +0530

    server.js

--- index.test.js
+++ index.test.js
@@ -3,10 +3,6 @@ const request = require('supertest');
 const app = require('./server'); // Import your Express app
 
 describe('Static Website Tests', () => {
-  afterAll((done) => {
-    server.close(done); // Close the server after tests to avoid Jest hanging
-  });
-  
   it('should respond with status 200 for the home page', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
@@ -16,5 +12,10 @@ describe('Static Website Tests', () => {
     const response = await request(app).get('/css/style.css'); // Assuming you have this file
     expect(response.status).toBe(200);
     expect(response.headers['content-type']).toMatch(/text\/css/);
+  });
+
+  it('should start the server and respond', (done) => {
+    expect(server.listening).toBeTruthy();
+    done();
   });
 });
